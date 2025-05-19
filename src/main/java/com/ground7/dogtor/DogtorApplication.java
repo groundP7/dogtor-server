debbfe5f7c96a3856c6550604cf49601ebeff3a1
groundP7 <p.ground714@gmail.com>
2025-05-19 12:42:17 +0900

    add

---
+++ src/main/java/com/ground7/dogtor/DogtorApplication.java
@@ -0,0 +1,13 @@
+package com.ground7.dogtor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogtorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogtorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ground7/dogtor/DogtorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ground7.dogtor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogtorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
